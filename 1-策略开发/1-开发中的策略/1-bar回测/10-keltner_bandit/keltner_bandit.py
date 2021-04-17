1ce2f44fa65d3f473cbdbb42e01db3e2bd405b01
huangning <hun1982@qq.com>
2021-04-17 08:23:49 +0800

    202104170823commited

---
+++ 1-策略开发/1-开发中的策略/1-bar回测/10-keltner_bandit/keltner_bandit.py
@@ -0,0 +1,283 @@
+#%%
+from typing import Any, Callable
+from vnpy.app.cta_strategy import (
+    CtaTemplate,
+    BarGenerator,
+    ArrayManager,
+    TradeData,
+    StopOrder,
+    OrderData
+)
+from vnpy.app.cta_strategy.base import StopOrderStatus, BacktestingMode
+from vnpy.app.cta_strategy.backtesting import BacktestingEngine, OptimizationSetting
+from vnpy.trader.object import BarData, TickData
+from vnpy.trader.constant import Interval, Offset, Direction, Exchange, Status
+import numpy as np
+import pandas as pd
+from datetime import time as time1  # 自定BarGenerator时，定义特殊时间点时会用到
+from datetime import datetime
+import time  # 用于测量某一段程序运行时间
+import talib
+import matplotlib.pyplot as plt
+import matplotlib as mpl
+from matplotlib import style
+mpl.rcParams['font.family'] = 'serif'  # 解决一些字体显示乱码的问题
+
+style.use('ggplot')
+import seaborn as sns
+
+sns.set()
+
+
+#%%
+class KeltnerBanditStrategyHNTest(CtaTemplate):
+    """"""
+    author = "Huang Ning"
+
+    kk_window = 20
+    kk_dev = 2
+    cci_window = 10
+    cci_stop = 44
+    atr_window = 10
+    risk_level = 5000
+
+    trading_size = 0
+    kk_up = 0
+    kk_down = 0
+    cci_value = 0
+    atr_value = 0
+
+    parameters = [
+        "kk_window",
+        "kk_dev",
+        "cci_window",
+        "cci_stop",
+        "atr_window",
+        "risk_level"
+    ]
+    variables = [
+        "trading_size",
+        "kk_up",
+        "kk_down",
+        "cci_value", 
+        "atr_value"
+    ]
+
+    def __init__(self, cta_engine, strategy_name, vt_symbol, setting):
+        """"""
+        super().__init__(cta_engine, strategy_name, vt_symbol, setting)
+
+        self.bg = XminBarGenerator(self.on_bar, 1, self.on_hour_bar, interval=Interval.HOUR)
+        self.am = ArrayManager()
+
+        self.buy_vt_orderids = []
+        self.sell_vt_orderids = []
+        self.short_vt_orderids = []
+        self.cover_vt_orderids = []
+
+    def on_init(self):
+        """"""
+        self.write_log("策略初始化")
+        self.load_bar(10)
+
+    def on_start(self):
+        """"""
+        self.write_log("策略启动")
+
+    def on_stop(self):
+        """"""
+        self.write_log("策略停止")
+
+    def on_tick(self, tick: TickData):
+        """"""
+        self.bg.update_tick(tick)
+
+    def on_bar(self, bar: BarData):
+        """"""
+        self.bg.update_bar(bar)
+
+    def on_hour_bar(self, bar: BarData):
+        """"""
+        am = self.am
+        am.update_bar(bar)
+        if not am.inited:
+            return
+
+        self.kk_up, self.kk_down = am.keltner(self.kk_window, self.kk_dev)
+        self.cci_value = am.cci(self.cci_window)
+
+        if self.pos == 0:
+            atr_value = am.atr(self.atr_window)
+            self.trading_size = max(int(self.risk_level / atr_value), 1)
+
+            self.buy(self.kk_up, self.trading_size, True)
+            self.short(self.kk_down, self.trading_size, True)
+
+        elif self.pos > 0:
+            if self.cci_value < - self.cci_stop:
+                self.sell(bar.close_price - 10, abs(self.pos), False)
+
+        elif self.pos < 0:
+            if self.cci_value > self.cci_stop:
+                self.cover(bar.close_price + 10, abs(self.pos), False)
+
+        self.put_event()
+
+    def on_order(self, order: OrderData):
+        """
+        Callback of new order data update.
+        """
+        pass
+
+    def on_trade(self, trade: TradeData):
+        """
+        Callback of new trade data update.
+        """
+        self.put_event()
+
+    def on_stop_order(self, stop_order: StopOrder):
+        """
+        Callback of stop order update.
+        """
+        pass
+
+
+class XminBarGenerator(BarGenerator):
+    def __init__(
+        self,
+        on_bar: Callable,
+        window: int = 0,
+        on_window_bar: Callable = None,
+        interval: Interval = Interval.MINUTE
+    ):
+        super().__init__(on_bar, window, on_window_bar, interval)
+    
+    def update_bar(self, bar: BarData) ->None:
+        """
+        Update 1 minute bar into generator
+        """
+        # If not inited, creaate window bar object
+        if not self.window_bar:
+            # Generate timestamp for bar data
+            if self.interval == Interval.MINUTE:
+                dt = bar.datetime.replace(second=0, microsecond=0)
+            else:
+                dt = bar.datetime.replace(minute=0, second=0, microsecond=0)
+
+            self.window_bar = BarData(
+                symbol=bar.symbol,
+                exchange=bar.exchange,
+                datetime=dt,
+                gateway_name=bar.gateway_name,
+                open_price=bar.open_price,
+                high_price=bar.high_price,
+                low_price=bar.low_price
+            )
+        # Otherwise, update high/low price into window bar
+        else:
+            self.window_bar.high_price = max(
+                self.window_bar.high_price, bar.high_price)
+            self.window_bar.low_price = min(
+                self.window_bar.low_price, bar.low_price)
+
+        # Update close price/volume into window bar
+        self.window_bar.close_price = bar.close_price
+        self.window_bar.volume += int(bar.volume)
+        self.window_bar.open_interest = bar.open_interest
+
+        # Check if window bar completed
+        finished = False
+
+        if self.interval == Interval.MINUTE:
+            # x-minute bar
+            # if not (bar.datetime.minute + 1) % self.window:
+            #     finished = True
+            
+            self.interval_count += 1
+
+            if not self.interval_count % self.window:
+                finished = True
+                self.interval_count = 0
+
+            elif bar.datetime.time() in [time1(10, 14), time1(11, 29), time1(14, 59), time1(22, 59)]:
+                if bar.exchange in [Exchange.SHFE, Exchange.DCE, Exchange.CZCE]:
+                    finished = True
+                    self.interval_count = 0
+
+        elif self.interval == Interval.HOUR:
+            if self.last_bar:
+                new_hour = bar.datetime.hour != self.last_bar.datetime.hour
+                last_minute = bar.datetime.minute == 59
+                not_first = self.window_bar.datetime != bar.datetime
+
+                # To filter duplicate hour bar finished condition
+                if (new_hour or last_minute) and not_first:
+                    # 1-hour bar
+                    if self.window == 1:
+                        finished = True
+                    # x-hour bar
+                    else:
+                        self.interval_count += 1
+
+                        if not self.interval_count % self.window:
+                            finished = True
+                            self.interval_count = 0
+
+        if finished:
+            self.on_window_bar(self.window_bar)
+            self.window_bar = None
+
+        # Cache last bar object
+        self.last_bar = bar
+
+#%%
+start1 = time.time()
+engine = BacktestingEngine()
+engine.set_parameters(
+    vt_symbol="rb888.SHFE",
+    interval="1m",
+    start=datetime(2020, 1, 1),
+    end=datetime(2021, 12, 31),
+    rate=0.0001,
+    slippage=1,
+    size=10,
+    pricetick=1,
+    capital=50000,
+    mode=BacktestingMode.BAR
+)
+engine.add_strategy(KeltnerBanditStrategyHNTest, {})
+
+
+#%%
+start2 = time.time()
+engine.load_data()
+end2 = time.time()
+print(f"加载数据所需时长: {(end2-start2)} Seconds")
+
+
+#%%
+engine.run_backtesting()
+
+
+#%%
+engine.calculate_result()
+engine.calculate_statistics()
+# 待测试的代码
+end1 = time.time()
+print(f"单次回测运行时长: {(end1-start1)} Seconds")
+engine.show_chart()
+
+
+#%%
+# setting = OptimizationSetting()
+# setting.set_target("end_balance")
+# setting.add_parameter("bar_window_length1", 1, 30, 1)
+# setting.add_parameter("bar_window_length2", 15, 45, 1)
+# setting.add_parameter("cci_window", 3, 33, 1)
+# setting.add_parameter("boll_window", 10, 30, 1)
+# # setting.add_parameter("cover_multiplier", 1.01, 1.20, 0.01)
+# setting.add_parameter("pricetick_multiplier", 1, 5, 1)
+
+
+#%%
+# engine.run_optimization(setting, output=True)
