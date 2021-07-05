cbdda6f10b82fb3fc4472187d01cd91543ea14f7
huangning <hun1982@qq.com>
2021-07-06 06:54:42 +0800

    202107060654committed

--- 9-脚本自动化/2-代码/no_ui/run_no_ui.py
+++ 9-脚本自动化/2-代码/no_ui/run_no_ui.py
@@ -9,8 +9,8 @@ from vnpy.trader.setting import SETTINGS
 from vnpy.trader.engine import MainEngine
 
 from vnpy.gateway.ctp import CtpGateway
-from vnpy.app.cta_strategy import CtaStrategyApp
-from vnpy.app.cta_strategy.base import EVENT_CTA_LOG
+from vnpy_ctastrategy import CtaStrategyApp
+from vnpy_ctastrategy.base import EVENT_CTA_LOG
 
 
 SETTINGS["log.active"] = True  # 是否记录日志
